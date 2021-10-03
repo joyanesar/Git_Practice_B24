d465115953d5285097221649b272242388099ffc
joyan <joyanisar@gmail.com>
2021-10-03 16:19:58 -0400

    updating

--- src/Setup/Car.java
+++ src/Setup/Car.java
@@ -2,18 +2,18 @@ package Setup;
 
 public class Car {
 
-<<<<<<< HEAD
+
     // this car class from master branch
 
     // we will have conflict
 
     // because there is different text in different branches for car
-=======
+
     //This is our car from james branch
 
     // more coming text
 
     // trying to merge jame but james file
 
->>>>>>> James
+
 }
