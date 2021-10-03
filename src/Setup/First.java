f806e291c3d7fc7b0955f3af9020ce327a25a226
joyan <joyanisar@gmail.com>
2021-10-03 11:31:29 -0400

    updating first file

---
+++ src/Setup/First.java
@@ -0,0 +1,15 @@
+package Setup;
+
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("Hell World");
+
+
+        //This is new line
+
+        // I want to creat a history
+
+        // The text doesnt matter
+    }
+}
